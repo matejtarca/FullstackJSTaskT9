a93406fbe9780eebb4040bdae4be31c3b40d8745
Matej Tarca <matejtarca@gmail.com>
2022-01-07 19:50:43 +0100

    added backspace function

--- t9ui/src/App.js
+++ t9ui/src/App.js
@@ -5,13 +5,20 @@ import { WordsList } from './components/WordsList';
 import Text from "@kiwicom/orbit-components/lib/Text";
 import Box from "@kiwicom/orbit-components/lib/Box";
 import Grid from "@kiwicom/orbit-components/lib/utils/Grid";
+import Stack from "@kiwicom/orbit-components/lib/Stack";
+import Button from "@kiwicom/orbit-components/lib/Button";
 import Layout, { LayoutColumn } from "@kiwicom/orbit-components/lib/Layout";
 import InputField from "@kiwicom/orbit-components/lib/InputField";
+import { ChevronLeft } from "@kiwicom/orbit-components/icons"
 import axios from 'axios';
 import { number } from 'prop-types';
 
 // Numbers that can contain letters in the keyboard
 const valueNumbers = ["2", "3", "4", "5", "6", "7", "8", "9"]
+
+function isNumeric(value) {
+  return /^-?\d+$/.test(value);
+}
 
 export function App() {
   const [numberSequence, setNumberSequence] = useState("");
@@ -20,9 +27,7 @@ export function App() {
   const [wordList, setWordList] = useState([]);
 
   useEffect(() => {
-    if (numberSequence) {
-      calculateText()
-    }
+    calculateText()
   }, [numberSequence])
 
 
@@ -35,31 +40,52 @@ export function App() {
   const handleWordClick = (word) => {
     setChosenWord(word);
   }
+
+  const handleDeleteClick = () => {
+    setNumberSequence(prevNumberSequence => prevNumberSequence.slice(0, prevNumberSequence.length - 1))
+  }
   
   const calculateText = () => {
-    axios.post("http://localhost:3000/parseNumbers", {
-        numbers: numberSequence,
-        useDict: useDict
-    })
-    .then((res) => {
-      const words = res.data.words
-      if (words) {
-        setChosenWord(words[0]);
-      } else {
-        setChosenWord("");
-      }
-      setWordList(words);
-    })
+    if (numberSequence) {
+      axios.post("http://localhost:3000/parseNumbers", {
+          numbers: numberSequence,
+          useDict: useDict
+      })
+      .then((res) => {
+        const words = res.data.words
+        if (words.length > 0) {
+          setChosenWord(words[0]);
+        } else {
+          setChosenWord(prevWord => {
+            const prevWordText = prevWord.replace(/\d+/, "")
+            return prevWordText + numberSequence.slice(prevWordText.length, numberSequence.length)
+          })
+        }
+        setWordList(words);
+      })
+    } else {
+      setChosenWord("");
+      setWordList([]);
+    }
+
   }
 
   return (
     <Layout type="Booking">
       <LayoutColumn>
-        <InputField 
-          readOnly={true}
-          value={chosenWord}
-        />
-        <Keyboard onKeyClick={handleKeyboardClick}/>    
+        <Stack
+          direction="row"
+          spacing="medium"
+        >
+          <InputField 
+            readOnly={true}
+            value={chosenWord}
+          />
+          <Button onClick={handleDeleteClick}><ChevronLeft /></Button>
+        </Stack>
+        <Box padding="XXXLarge">
+          <Keyboard onKeyClick={handleKeyboardClick}/>    
+        </Box>
       </LayoutColumn>
       <LayoutColumn>
         <WordsList words={wordList} onWordClick={handleWordClick}/>
